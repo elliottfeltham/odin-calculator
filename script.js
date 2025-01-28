3faed982e2d0b802f3fd962fe9498cde06384b2d
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-28 19:01:27 +0000

    Add functions for maths operators

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Addition function 
 
+const add = function(a, b) {
+    return a + b;
+}
+
 // Subtraction function 
+
+
+const subtract = function(a , b) {
+    return a - b;
+}
 
 // Multiplication function 
 
-// Division function 
+const multiply = function(a, b) {
+    return a * b;
+}
+
+// Division function
+
+const divide = function( a, b) {
+    return a / b; 
+}
+
+console.log(add(10, 2));
+console.log(subtract(10, 2));
+console.log(multiply(10, 2));
+console.log(divide(10, 2));
